0c8300dbea3198bae600c7777d044ef85310bc06
Saar Haber <37675905+saarhaber@users.noreply.github.com>
2021-04-25 13:23:09 -0400

    splash and auth added, goes to MainActivity and showed recently played song with option to add to liked songs

--- app/src/main/java/com/example/melocommunity/MainActivity.java
+++ app/src/main/java/com/example/melocommunity/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.melocommunity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.melocommunity.Connectors.SongService;
+import com.example.melocommunity.models.Song;
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
@@ -14,64 +20,111 @@ import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String CLIENT_ID = "676d4db0d44b4f95956d8efa0ff25ff8";
-    private static final String REDIRECT_URI = "com.example.melocommunity://callback";
-    private SpotifyAppRemote mSpotifyAppRemote;
+    private TextView userView;
+    private TextView songView;
+    private Button addBtn;
+    private Song song;
 
+    private SongService songService;
+    private ArrayList<Song> recentlyPlayedTracks;
+
+//    private static final String CLIENT_ID = "676d4db0d44b4f95956d8efa0ff25ff8";
+//    private static final String REDIRECT_URI = "com.example.melocommunity://callback";
+//    private SpotifyAppRemote mSpotifyAppRemote;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        songService = new SongService(getApplicationContext());
+        userView = (TextView) findViewById(R.id.user);
+        songView = (TextView) findViewById(R.id.song);
+        addBtn = (Button) findViewById(R.id.add);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        userView.setText(sharedPreferences.getString("userid", "No User"));
+
+        getTracks();
+
+        addBtn.setOnClickListener(addListener);
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        ConnectionParams connectionParams =
-                new ConnectionParams.Builder(CLIENT_ID)
-                        .setRedirectUri(REDIRECT_URI)
-                        .showAuthView(true)
-                        .build();
+    private final View.OnClickListener addListener = v -> {
+        songService.addSongToLibrary(this.song);
+        if (recentlyPlayedTracks.size() > 0) {
+            recentlyPlayedTracks.remove(0);
+        }
+        updateSong();
+    };
 
-        SpotifyAppRemote.connect(this, connectionParams,
-                new Connector.ConnectionListener() {
 
-                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-                        mSpotifyAppRemote = spotifyAppRemote;
-                        Log.d("MainActivity", "Connected! Yay!");
-
-                        // Now you can start interacting with App Remote
-                        connected();
-
-                    }
-
-                    public void onFailure(Throwable throwable) {
-                        Log.e("MyActivity", throwable.getMessage(), throwable);
-
-                        // Something went wrong when attempting to connect! Handle errors here
-                    }
-                });
+    private void getTracks() {
+        songService.getRecentlyPlayedTracks(() -> {
+            recentlyPlayedTracks = songService.getSongs();
+            updateSong();
+        });
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    private void updateSong() {
+        if (recentlyPlayedTracks.size() > 0) {
+            songView.setText(recentlyPlayedTracks.get(0).getName());
+            song = recentlyPlayedTracks.get(0);
+        }
     }
 
-    private void connected() {
-        // Play a playlist
-        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
-
-        // Subscribe to PlayerState
-        mSpotifyAppRemote.getPlayerApi()
-                .subscribeToPlayerState()
-                .setEventCallback(playerState -> {
-                    final Track track = playerState.track;
-                    if (track != null) {
-                        Log.d("MainActivity", track.name + " by " + track.artist.name);
-                    }
-                });
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        ConnectionParams connectionParams =
+//                new ConnectionParams.Builder(CLIENT_ID)
+//                        .setRedirectUri(REDIRECT_URI)
+//                        .showAuthView(true)
+//                        .build();
+//
+//        SpotifyAppRemote.connect(this, connectionParams,
+//                new Connector.ConnectionListener() {
+//
+//                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+//                        mSpotifyAppRemote = spotifyAppRemote;
+//                        Log.d("MainActivity", "Connected! Yay!");
+//
+//                        // Now you can start interacting with App Remote
+//                        connected();
+//
+//                    }
+//
+//                    public void onFailure(Throwable throwable) {
+//                        Log.e("MyActivity", throwable.getMessage(), throwable);
+//
+//                        // Something went wrong when attempting to connect! Handle errors here
+//                    }
+//                });
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+//    }
+//
+//    private void connected() {
+//        // Play a playlist
+//        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+//
+//        // Subscribe to PlayerState
+//        mSpotifyAppRemote.getPlayerApi()
+//                .subscribeToPlayerState()
+//                .setEventCallback(playerState -> {
+//                    final Track track = playerState.track;
+//                    if (track != null) {
+//                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+//                    }
+//                });
+//    }
 }
