7a37456ebea6ba5c787f0eb2d55dbf6cd1932675
Saar Haber <37675905+saarhaber@users.noreply.github.com>
2021-04-27 20:09:53 -0400

    merge main with fragments

--- app/src/main/java/com/example/melocommunity/MainActivity.java
+++ app/src/main/java/com/example/melocommunity/MainActivity.java
@@ -5,49 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
+
 import android.os.Bundle;
 
-import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.melocommunity.fragments.AccountFragment;
 import com.example.melocommunity.fragments.FeedFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
-import com.example.melocommunity.Connectors.SongService;
-import com.example.melocommunity.models.Song;
-
-import com.spotify.android.appremote.api.ConnectionParams;
-import com.spotify.android.appremote.api.Connector;
-import com.spotify.android.appremote.api.SpotifyAppRemote;
-
-import com.spotify.protocol.client.Subscription;
-import com.spotify.protocol.types.PlayerState;
-import com.spotify.protocol.types.Track;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String CLIENT_ID = "676d4db0d44b4f95956d8efa0ff25ff8";
-    private static final String REDIRECT_URI = "com.example.melocommunity://callback";
-    private SpotifyAppRemote mSpotifyAppRemote;
 
     final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
@@ -76,127 +45,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         // Set default selection
-        bottomNavigationView.setSelectedItemId(R.id.action_feed);
-    }
-//to move from here
-    private TextView userView;
-    private TextView songView;
-    private Button addBtn;
-    private Song song;
-    private ImageView imageView;
-    private Context context;
-
-    private SongService songService;
-    private ArrayList<Song> recentlyPlayedTracks;
-
-//    private static final String CLIENT_ID = "676d4db0d44b4f95956d8efa0ff25ff8";
-//    private static final String REDIRECT_URI = "com.example.melocommunity://callback";
-//    private SpotifyAppRemote mSpotifyAppRemote;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        songService = new SongService(getApplicationContext());
-        userView = (TextView) findViewById(R.id.user);
-        songView = (TextView) findViewById(R.id.song);
-        addBtn = (Button) findViewById(R.id.add);
-        imageView = (ImageView) findViewById(R.id.imageProfile);
-
-
-        SharedPreferences sharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
-        userView.setText(sharedPreferences.getString("display_name", "No User"));
-        String url = sharedPreferences.getString("imageUrl", "No User");
-
-        RequestOptions options = new RequestOptions()
-                .centerCrop()
-                .placeholder(R.mipmap.ic_launcher_round)
-                .error(R.mipmap.ic_launcher_round);
-
-        Glide.with(this)
-                .load(url)
-                .apply(options)
-                .into(imageView );
-
-        //Log.i("MainActivity", sharedPreferences.getAll().toString());
-
-        getTracks();
-
-        addBtn.setOnClickListener(addListener);
-
+        bottomNavigationView.setSelectedItemId(R.id.action_account);
     }
 
-    private final View.OnClickListener addListener = v -> {
-        songService.addSongToLibrary(this.song);
-        if (recentlyPlayedTracks.size() > 0) {
-            recentlyPlayedTracks.remove(0);
-        }
-        updateSong();
-    };
-
-
-    private void getTracks() {
-        songService.getRecentlyPlayedTracks(() -> {
-            recentlyPlayedTracks = songService.getSongs();
-            updateSong();
-        });
-    }
-
-    private void updateSong() {
-        if (recentlyPlayedTracks.size() > 0) {
-            songView.setText("Last song played: " + recentlyPlayedTracks.get(0).getName());
-            song = recentlyPlayedTracks.get(0);
-        }
-    }
-
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        ConnectionParams connectionParams =
-//                new ConnectionParams.Builder(CLIENT_ID)
-//                        .setRedirectUri(REDIRECT_URI)
-//                        .showAuthView(true)
-//                        .build();
-//
-//        SpotifyAppRemote.connect(this, connectionParams,
-//                new Connector.ConnectionListener() {
-//
-//                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-//                        mSpotifyAppRemote = spotifyAppRemote;
-//                        Log.d("MainActivity", "Connected! Yay!");
-//
-//                        // Now you can start interacting with App Remote
-//                        connected();
-//
-//                    }
-//
-//                    public void onFailure(Throwable throwable) {
-//                        Log.e("MyActivity", throwable.getMessage(), throwable);
-//
-//                        // Something went wrong when attempting to connect! Handle errors here
-//                    }
-//                });
-//    }
-//
-//    @Override
-//    protected void onStop() {
-//        super.onStop();
-//        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
-//    }
-//
-//    private void connected() {
-//        // Play a playlist
-//        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
-//
-//        // Subscribe to PlayerState
-//        mSpotifyAppRemote.getPlayerApi()
-//                .subscribeToPlayerState()
-//                .setEventCallback(playerState -> {
-//                    final Track track = playerState.track;
-//                    if (track != null) {
-//                        Log.d("MainActivity", track.name + " by " + track.artist.name);
-//                    }
-//                });
-//    }
 }
--- app/src/main/java/com/example/melocommunity/fragments/AccountFragment.java
+++ app/src/main/java/com/example/melocommunity/fragments/AccountFragment.java
@@ -1,5 +1,7 @@
 package com.example.melocommunity.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,13 +11,32 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.melocommunity.Connectors.SongService;
 import com.example.melocommunity.R;
+import com.example.melocommunity.models.Song;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AccountFragment extends Fragment {
+
+    private TextView userView;
+    private TextView songView;
+    private Button addBtn;
+    private Song song;
+    private ImageView imageView;
+    private Context context;
+
+    private SongService songService;
+    private ArrayList<Song> recentlyPlayedTracks;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -37,5 +58,57 @@ public class AccountFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        songService = new SongService(getContext().getApplicationContext());
+        userView = view.findViewById(R.id.user);
+        songView = view.findViewById(R.id.song);
+        addBtn = view.findViewById(R.id.add);
+        imageView = view.findViewById(R.id.imageProfile);
+
+
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("SPOTIFY", 0);
+        userView.setText(sharedPreferences.getString("display_name", "No User"));
+        String url = sharedPreferences.getString("imageUrl", "No User");
+
+        RequestOptions options = new RequestOptions()
+                .centerCrop()
+                .placeholder(R.mipmap.ic_launcher_round)
+                .error(R.mipmap.ic_launcher_round);
+
+        Glide.with(this)
+                .load(url)
+                .apply(options)
+                .into(imageView );
+
+        //Log.i("MainActivity", sharedPreferences.getAll().toString());
+
+        getTracks();
+
+        addBtn.setOnClickListener(addListener);
+
     }
+
+    private final View.OnClickListener addListener = v -> {
+        songService.addSongToLibrary(this.song);
+        if (recentlyPlayedTracks.size() > 0) {
+            recentlyPlayedTracks.remove(0);
+        }
+        updateSong();
+    };
+
+
+    private void getTracks() {
+        songService.getRecentlyPlayedTracks(() -> {
+            recentlyPlayedTracks = songService.getSongs();
+            updateSong();
+        });
+    }
+
+    private void updateSong() {
+        if (recentlyPlayedTracks.size() > 0) {
+            songView.setText("Last song played: " + recentlyPlayedTracks.get(0).getName());
+            song = recentlyPlayedTracks.get(0);
+        }
+    }
+
 }
