09ed3402898bf310dd7d8a1fc012eb3dc92c0445
TienNguyen93 <nguyentien9933@gmail.com>
2021-05-08 21:19:13 -0400

    change feedsongs dummy to parse model

--- app/src/main/java/com/example/melocommunity/models/FeedSongs.java
+++ app/src/main/java/com/example/melocommunity/models/FeedSongs.java
@@ -1,24 +1,54 @@
 package com.example.melocommunity.models;
 
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
 import com.spotify.protocol.types.Image;
 
 import java.util.Date;
 
-public class FeedSongs {
+@ParseClassName("FeedSongs")
+public class FeedSongs extends ParseObject {
 
-    public String songID;
-    public String nameSong;
-    public String nameArtist;
-    public Image imageSong;
-    public Date createdAt;
+    public static final String KEY_SONG_ID = "songID";
+    public static final String KEY_NAME_SONG = "nameSong";
+    public static final String KEY_NAME_ARTIST = "nameArtist";
+    public static final String KEY_IMAGE_SONG = "imageSong";
+    //public static final String KEY_CREATED_AT = "createdAt";
 
-    public FeedSongs(String songID, String nameSong, String nameArtist,
-                     Image imageSong, Date createdAt) {
+    //songID
+    public String getSongID() {
+        return getString(KEY_SONG_ID);
+    }
 
-        this.songID = songID;
-        this.nameSong = nameSong;
-        this.nameArtist = nameArtist;
-        this.imageSong = imageSong;
-        this.createdAt = createdAt;
+    public void setSongID(String songID) {
+        put(KEY_SONG_ID, songID);
+    }
+
+    //nameSong
+    public String getNameSong() {
+        return getString(KEY_NAME_SONG);
+    }
+
+    public void setNameSong(String nameSong) {
+        put(KEY_NAME_SONG, nameSong);
+    }
+
+    //nameArtist
+    public String getNameArtist() {
+        return getString(KEY_NAME_ARTIST);
+    }
+
+    public void setNameArtist(String nameArtist) {
+        put(KEY_NAME_ARTIST, nameArtist);
+    }
+
+    public ParseFile getImage() {
+        return getParseFile(KEY_IMAGE_SONG);
+    }
+
+    public void setImage(ParseFile parseFile) {
+        put(KEY_IMAGE_SONG, parseFile);
     }
 }
+
